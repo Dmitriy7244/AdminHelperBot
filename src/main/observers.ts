8c86a9e3049126c219a45ed1938ff5421693289b
levch <levchenko.d.a1998@gmail.com>
2023-07-08 21:33:06 +0600

    fix access

--- src/main/observers.ts
+++ src/main/observers.ts
@@ -1,5 +1,19 @@
+import { NextFunction } from "grammy"
 import { observer } from "observer"
+import { Context } from "types"
+import { bot } from "../core/bot.ts"
 import { Prefix } from "./buttons.ts"
+import { REPORT_CHAT_ID } from "./config.ts"
+
+async function checkAccess(ctx: Context, next: NextFunction) {
+  if (ctx.chat?.id != REPORT_CHAT_ID) {
+    if (ctx.chat) await ctx.reply("Мне запрещено общаться в этом чате")
+    return
+  }
+  await next()
+}
+
+bot.use(checkAccess)
 
 const button = (prefix: Prefix) => observer.button(prefix)
 
