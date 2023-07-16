dcd2f624748896760a34c4d69186edf8aff4ea99
levch <levchenko.d.a1998@gmail.com>
2023-07-16 20:51:47 +0600

    -

--- src/main/config.ts
+++ src/main/config.ts
@@ -9,7 +9,7 @@ const _channelsData = [
   [-1001592432164, "Крутые авы", "https://t.me/anime4_avatarki"],
   [-1001271558289, "Мультач", "https://t.me/multtach"],
   [-1001563981037, "Пикзери", "https://t.me/pikzery"],
-  [-1001988740515, "Пошлые опросы", "https://t.me/+CftFBjqS5pc2ZTk6"],
+  [-1001988740515, "Пошлые опросы", "https://t.me/+MaAfA3CeThowYmQy"],
   [-1001554954940, "Кошачья лежанка", "https://t.me/+fFddn0cMxa5hZGI6"],
   [-1001573250306, "Стикеры", "https://t.me/anime4_arts"],
   [-1001894750175, "Счастье-это", "https://t.me/segofniii"],
@@ -19,7 +19,7 @@ const _channelsData = [
   [-1001217147231, "Фонк", "https://t.me/+Bc3EFuWbUhpjMTEy"],
   [-1001671652919, "Обои", "https://t.me/wallposter_r"],
   [-1001942180236, "ЯОЙ", "https://t.me/+XnJ8RZIXcfAyYjVi"],
-  [-1001585027208, "Тест", "https://t.me/test7244c"],
+  // [-1001585027208, "Тест", "https://t.me/test7244c"],
 ] as const
 
 interface Channel {
