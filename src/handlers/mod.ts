7727861ac0382073b207017944da2430b91db25a
levch <levchenko.d.a1998@gmail.com>
2023-07-29 13:05:04 +0600

    -

--- src/handlers/mod.ts
+++ src/handlers/mod.ts
@@ -47,7 +47,7 @@ O.channelPost.handler = async (ctx) => {
 }
 
 const AD_DURATION = 60 * 60 * 24
-const AD_TOP_DURATION = 60 * 60 * 0 // TODO
+const AD_TOP_DURATION = 60 * 60 // TODO
 
 O.checkRights.handler = async (ctx) => {
   const noRightsChannels = []
